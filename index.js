8be6bbfefd7003ac8d4213678c277cb9f4dff2d5
Jared Johnson <jared@nuts.com>
2019-05-01 13:12:36 -0400

    Use external template in index.js.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-// Create vue instance
 const Vue = require('vue');
 const vsr = require('vue-server-renderer');
+const fs = require('fs');
+const server = require('express')();
 
-const app = new Vue({
-  template: `<div>Hello, world!</div>`,
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url,
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`,
+  });
+
+  const renderer = vsr.createRenderer({
+    template: fs.readFileSync('./index.template.html', 'utf-8'),
+  });
+
+  renderer.renderToString(app).then((html) => {
+    res.end(html);
+  }).catch((error) => {
+    console.error(error);
+    res.status(500).end('Internal Server Error');
+  });
 });
 
-// Render vue into HTML
-const renderer = vsr.createRenderer();
-renderer.renderToString(app).then((html) => {
-  console.log(html);
-}).catch((error) => {
-  console.error(error);
-});
+server.listen(8080);
