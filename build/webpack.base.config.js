5d0d13e3d149193d02317e5bc98bbb68887e6293
Jared Johnson <jared@nuts.com>
2019-05-02 17:00:36 -0400

    Flesh out webpack.base.config.

---
+++ build/webpack.base.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
+// conditional configuration
+const config = {};
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.plugins = [
+    new VueLoaderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {warnings: false},
+    }),
+    new ExtractTextPlugin({
+      filename: 'common.[chunkhash].css',
+    }),
+  ];
+} else {
+  config.devtool = '#cheap-module-source-map';
+  config.plugins = [new VueLoaderPlugin()];
+}
+
+module.exports = {
+  devtool: config.devtool,
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: '[name].[chunkhash].js',
+  },
+  resolve: {
+    alias: {
+      public: path.resolve(__dirname, '../public'),
+    },
+  },
+  module: {
+    noParse: /es6-promise\.js$/, // avoid webpack shimming process
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: config.plugins,
+};
