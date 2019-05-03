5e8d45b47ec38da5031d187a299f9799a8aba9fb
Jared Johnson <jared@nuts.com>
2019-05-03 15:36:35 -0400

    Flesh out App.vue with routes.

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
-    I AM APP
+    <header>
+      <nav>
+        <router-link to="/">home</router-link>
+        <router-link to="/foo">foo</router-link>
+        <router-link to="/bar">bar</router-link>
+        <router-link to="/baz">baz</router-link>
+      </nav>
+    </header>
+    <router-view></router-view>
   </div>
 </template>
