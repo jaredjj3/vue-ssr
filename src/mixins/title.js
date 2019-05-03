27239086a95893419da3f75d6434bdf2ebee113e
Jared Johnson <jared@nuts.com>
2019-05-03 14:34:43 -0400

    Create title mixin.

---
+++ src/mixins/title.js
@@ -0,0 +1,30 @@
+const getTitle = (vm) => {
+  const {title} = vm.$options;
+  if (title) {
+    return typeof title === 'function'
+      ? title.call(vm)
+      : title;
+  }
+};
+
+const serverTitleMixin = {
+  created() {
+    const title = getTitle(this);
+    if (title) {
+      this.$ssrContext.title = `vue-ssr | ${title}`;
+    }
+  },
+};
+
+const clientTitleMixin = {
+  mounted() {
+    const title = getTitle(this);
+    if (title) {
+      document.title = `vue-ssr | ${title}`;
+    }
+  },
+};
+
+export default process.env.VUE_ENV === 'server'
+  ? serverTitleMixin
+  : clientTitleMixin;
