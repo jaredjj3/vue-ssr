b392409f16ba7d12a45d589586432f57b9fe6411
Jared Johnson <jared@nuts.com>
2019-05-03 15:58:38 -0400

    Create client entry.

---
+++ src/entry-client.js
@@ -0,0 +1,16 @@
+import 'es6-promise/auto';
+import {createApp} from './app';
+
+const {app, router, store} = createApp();
+
+// prime the store with server-initialized state.
+// the state is determined during SSR and inlined in the page markup.
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__)
+}
+
+// wait until router has resolved all async before hooks
+// and async components...
+router.onReady(() => {
+  app.$mount('#app');
+});
