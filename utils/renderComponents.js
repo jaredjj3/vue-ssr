f1aa47ad7906fa800e4d1b606348a947bde4166e
Jared Johnson <jared@nuts.com>
2019-05-14 11:02:47 -0400

    Document renderComponents helpers.

--- utils/renderComponents.js
+++ utils/renderComponents.js
@@ -1,11 +1,23 @@
 const { createBundleRenderer } = require('vue-server-renderer');
 const createHydrationSpec = require('./createHydrationSpec');
 
+/**
+ * @param {Component} component
+ * @return {boolean}
+ */
 const isAssetsComponent = (component) => {
   return component.componentName === 'Assets';
 };
 
-const getAssetsTemplate = (components, getStore) => (_html, context) => {
+/**
+ * Creates an array of hydration specs, which are sent to the client to
+ * instruct it how to hydrate each component. If a component is an
+ * assets component, it is skipped since it isn't valid to hydrate it.
+ *
+ * @param {Component[]} components
+ * @return {HydrationSpec[]}
+ */
+const getHydrationSpecs = (components) => {
   const hydrationSpecs = [];
   for (const component of components) {
     if (!isAssetsComponent(component)) {
@@ -13,49 +25,96 @@ const getAssetsTemplate = (components, getStore) => (_html, context) => {
       hydrationSpecs.push(hydrationSpec);
     }
   }
+  return hydrationSpecs;
+};
 
-  context.state = getStore().state;
-  const windowInitialState = context.renderState({
-    windowKey: '__INITIAL_STATE__',
-  });
+/**
+ * In order to manually inject assets, the context state must be set, then
+ * context.renderState() must be called. The purpose of this function is
+ * to abstract this complexity away and to ensure the context.state attribute
+ * is unchanged.
+ *
+ * See https://ssr.vuejs.org/api/#template and
+ * https://ssr.vuejs.org/guide/build-config.html#manual-asset-injection for
+ * more information.
+ *
+ * @param {object} context
+ * @param {string} windowKey
+ * @param {any} state
+ * @return {string}
+ */
+const renderState = (context, windowKey, state) => {
+  const tmp = context.state;
+  context.state = state;
+  const script = context.renderState({ windowKey });
+  context.state = tmp;
+  return script;
+};
 
-  context.state = hydrationSpecs;
-  const windowHydrationSpecs = context.renderState({
-    windowKey: '__HYDRATION_SPECS__',
-  });
+/**
+ * Returns a function that can be used in the createBundleRenderer template
+ * options object. This template includes the scripts needed to properly
+ * render the assets component. This template should only be used for the
+ * assets template.
+ *
+ * @param {Component[]} components
+ * @param {function} getStore
+ * @return {function}
+ */
+const getAssetsTemplate = (components, getStore) => (_html, context) => {
+  const windowInitialStateScript = renderState(
+      context,
+      '__INITIAL_STATE__',
+      getStore().state
+  );
 
-  const scripts = context.renderScripts();
+  const windowHydrationSpecsScript = renderState(
+      context,
+      '__HYDRATION_SPECS__',
+      getHydrationSpecs(components)
+  );
+
+  const clientBundleScripts = context.renderScripts();
 
   return [
-    windowInitialState,
-    windowHydrationSpecs,
-    scripts,
+    windowInitialStateScript,
+    windowHydrationSpecsScript,
+    clientBundleScripts,
   ].join('\n');
 };
 
+/**
+ * The purpose of this function is to populate the html attribute in-place
+ * of the components argument.
+ *
+ * @param {ServerBundle} bundle
+ * @param {ClientManifest} clientManifest
+ * @param {Component[]} components
+ * @return {Promise<void>}
+ */
 module.exports = (bundle, clientManifest, components) => {
-  const renders = [];
-
-  // set store
-  const f = () => {
+  // This stateful function is used to only create the store once
+  // It can be called with or without a createStore function
+  const getStore = (() => {
     let store;
-    return (createStore) => {
+    const noop = () => undefined;
+    return (createStore = noop) => {
       store = store || createStore();
       return store;
     };
-  };
-  const getStore = f();
+  })();
 
+  // Populate the renderPromises array
+  const renderPromises = [];
   for (const component of components) {
-    const template = isAssetsComponent(component)
-      ? getAssetsTemplate(components, getStore)
-      : undefined;
-
+    let template; // ok to be undefined
+    if (isAssetsComponent(component)) {
+      template = getAssetsTemplate(components, getStore);
+    }
     const renderer = createBundleRenderer(bundle, {
       clientManifest,
       template,
     });
-
     const context = {
       ...component,
       getStore,
@@ -64,9 +123,8 @@ module.exports = (bundle, clientManifest, components) => {
     const renderPromise = renderer.renderToString(context).then((html) => {
       component.html = html;
     });
-
-    renders.push(renderPromise);
+    renderPromises.push(renderPromise);
   }
 
-  return Promise.all(renders);
+  return Promise.all(renderPromises);
 };
